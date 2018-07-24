8c6e645b40d2299a00dca963cb219433f647fede
Omniprezz <geoffrey.frioli@gmail.com>
2018-07-24 21:45:08 +0200

    test css

--- parts/menu.php
+++ parts/menu.php
@@ -1,7 +1,7 @@
 
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">My Portfolio</a>
+  <a class="navbar-brand" id="titre" href="#">My Portfolio</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
