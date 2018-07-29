a032af8a808e0393ef9d5d38817d75e883f61e5c
Omniprezz <geoffrey.frioli@gmail.com>
2018-07-29 10:33:27 +0200

    update responsiv css

--- pages/home.php
+++ pages/home.php
@@ -20,17 +20,29 @@
 
 <div id="start" class="container-fluid">
     <div class="row menuCard">
-        <a href="#" class="col-3 card flex">
+        <a href="#" class="col-xs-12 col-sm-12 col-md-3 col-lg-3 col-xl-3 card flex">
             Site Web...
         </a>
-        <a href="#" class="col-3 card flex flex1">
+        <a href="#" class="col-xs-12 col-sm-12 col-md-3 col-lg-3 col-xl-3 card flex flex1">
             Apply...
         </a>
-        <a href="#" class="col-3 card flex flex2">
+        <a href="#" class="col-xs-12 col-sm-12 col-md-3 col-lg-3 col-xl-3 card flex flex2">
             <span>Design...</span>
         </a>
     </div>
+    <div class="row">
+        <div class="arrow1 text-center col-12">
+            <a href="#next" class="js-scrollTo">
+                <i class="icoArrow material-icons">
+                keyboard_arrow_down
+                </i>
+            </a>
+        </div>
+    </div>
 </div>
 
+<div id="next" class="container-fluid">
+
+</div>
 
 
